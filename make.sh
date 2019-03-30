de9089d149f990218332d506760498edb783caec
root <Fyodorov.aleksej@gmail.com>
2019-03-30 11:19:33 +0300

    [Docker_update] - Adding make sh script for invoke makefile in tex folder. Update docker_run for attach to container

--- make.sh
+++ make.sh
@@ -9,4 +9,6 @@ docker stop $NAME
 docker rm $NAME
 
 docker build -t $IMAGE .
-docker run -it --name $NAME -v $HOST_FOLDER:$CONTAINER_FOLDER -w $CONTAINER_FOLDER $IMAGE
+docker run --name $NAME -v $HOST_FOLDER:$CONTAINER_FOLDER -w $CONTAINER_FOLDER $IMAGE $@
+docker stop $NAME
+docker rm $NAME
